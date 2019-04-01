d4cd64775e1fe9621a0489b6d4d447908005fca2
Joel Arroyo <joelarroyo1203@gmail.com>
2019-03-31 23:12:56 -0400

    commit

---
+++ SpaceShooterTut/Assets/DestroyByContact.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController1 gameController;
+    
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController1");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController1>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController1' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
+
+
+
+
+
+
+
+/*
+using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController1 gameController;
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController1");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController1>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController1' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            audio.Play();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
+*/
---
+++ SpaceShooterTut/Assets/DestroyByTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+    private AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+        Destroy(gameObject, lifetime);
+
+    }
+}
